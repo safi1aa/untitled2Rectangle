b69ea6151d86701a24a7117f393f36f5cc3a706f
User <egemberdievasafia@gmail.com>
2023-06-12 19:10:42 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setA(10);
+        rectangle.setB(15);
+        rectangle.setC(10);
+        rectangle.setD(15);
+        rectangle.square();
+    }
+}
